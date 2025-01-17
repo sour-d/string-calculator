5d9ba20afe6f4850e65869e463eff59034fa5621
sourav das <36912347+sour-d@users.noreply.github.com>
2025-01-17 22:52:21 +0530

    Added feature to calculate sum of 2 numbers separated by comma

--- src/add.js
+++ src/add.js
@@ -2,9 +2,12 @@ const add = (arg) => {
   if (arg === "") {
     return 0;
   }
-  if (arg.match(/[0-9]/)) {
+  if (arg.match(/^[0-9]*$/)) {
     return arg;
   }
+
+  const [num1, num2] = arg.split(",").map((num) => parseInt(num));
+  return num1 + num2;
 };
 
 export default add;
--- test/add.test.js
+++ test/add.test.js
@@ -18,4 +18,26 @@ describe("add", () => {
       expect(add(arg)).toBe(arg);
     }
   );
+
+  it.each([
+    ["1,2", 3],
+    ["3,4", 7],
+    ["5,3", 8],
+  ])(
+    "should return the sum of numbers if 2 numbers are is comma separated",
+    (arg, expected) => {
+      expect(add(arg)).toBe(expected);
+    }
+  );
+
+  it.each([
+    ["13,25", 38],
+    ["3,45", 48],
+    ["31,3", 34],
+  ])(
+    "should return the sum of numbers of 2 numbers having multiple digits and comma separated",
+    (arg, expected) => {
+      expect(add(arg)).toBe(expected);
+    }
+  );
 });
