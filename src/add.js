bc6fe7c4a054910944216d44afc751e292b32d25
sourav das <36912347+sour-d@users.noreply.github.com>
2025-01-17 21:58:47 +0530

    Added single number calculation feature

--- src/add.js
+++ src/add.js
@@ -2,6 +2,9 @@ const add = (arg) => {
   if (arg === "") {
     return 0;
   }
+  if (arg.length === 1 && arg[0].match(/[0-9]/)) {
+    return arg;
+  }
 };
 
 export default add;
--- test/add.test.js
+++ test/add.test.js
@@ -4,4 +4,11 @@ describe("add", () => {
   it("should return 0 if argument is a empty string", () => {
     expect(add('')).toBe(0);
   });
+
+  it.each(["1", "2", "3"])(
+    "should return the number if the argument is a signle number",
+    (arg) => {
+      expect(add(arg)).toBe(arg);
+    }
+  );
 });
